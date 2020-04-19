104fdf5dbc915cd9df3d8a9aa408634efa3f49b5
Jordan Gaslonde <J.Gaslonde@outlook.com>
2020-04-19 19:52:50 +0100

    stiff

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -6,7 +6,29 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int yesCounter = 1;
+            int noCounter = 2;
+
+            int total = yesCounter + noCounter;
+
+            var yesPercent = yesCounter / total * 100.0;
+            var noPercent = noCounter / total * 100.0;
+
+            Console.WriteLine($"Yes Counts: {yesCounter}, Percentage: {yesPercent}%");
+            Console.WriteLine($"No Counts: {noCounter}, Percentage: {noPercent}%");
+
+            if (yesCounter > noCounter)
+            {
+                Console.WriteLine($"Yes Won");
+            }
+            else if (noCounter > yesCounter)
+            {
+                Console.WriteLine($"No Won");
+            }
+            else
+            {
+                Console.WriteLine($"Draw");
+            }
         }
     }
 }
